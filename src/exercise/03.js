9f75e0210448fc85eec3dbdf3ef18da70f5683fb
Andrew Davis <andrew4d3@gmail.com>
2023-12-24 12:38:07 -0300

    Lesson 54: Colocating state

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,12 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input id="name" value={name} onChange={e => setName(e.target.value)} />
     </div>
   )
 }
@@ -32,21 +33,20 @@ function FavoriteAnimal({animal, changeAnimal}) {
 // }
 
 // 💣 remove this component in favor of the new one
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
-  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
 
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal animal={animal} changeAnimal={setAnimal} />
       {/* 🐨 pass the animal prop here */}
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
